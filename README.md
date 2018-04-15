4227b18b63224902f84acf3294983734da232f69
maikimo <mike.p.monahan@gmail.com>
2018-04-15 19:45:34 -0400

    add to hash tut

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ruby Tutorials
+Ruby Method/Object Tutorials based upon the Jake Day Williams youtube series.  Started halfway through as most the similarities up to this point with Javascript were redundant.
--- add-to-hash.rb
+++ add-to-hash.rb
@@ -6,22 +6,24 @@ novels = {
 
 puts "Here is your current list of novels: #{novels}"
 
-puts "Would you like to add another book to your hash? (Type yes or no)"
-
-answer = gets.chomp
-answer.downcase!
-
-case answer
-    when "yes"
-        puts "What book would you like to add?"
-        book = gets.chomp
-        novels[book.to_sym]
-        puts "What rating would you like to give this book?[1 - 10]"
-        rating = gets.chomp
-        novels[book.to_sym] = rating.to_i
-        puts "#{book} has been added to your library with a rating of #{rating}"
-    when "no"
-        puts "Sounds good to me!"
+answer = "yes"
+while answer == "yes"
+    puts "Would you like to add another book to your hash? (Type yes or no)"
+    answer = gets.chomp
+    answer.downcase!
+    case answer
+        when "yes"
+            puts "What book would you like to add?"
+            book = gets.chomp
+            novels[book.to_sym]
+            puts "What rating would you like to give this book?[1 - 10]"
+            rating = gets.chomp
+            novels[book.to_sym] = rating.to_i
+            puts "#{book} has been added to your library with a rating of #{rating}"
+            answer == ""
+        when "no"
+            puts "Sounds good to me!"
+    end
 end
 
 puts "Here is what is in your hash right now: #{novels}"
